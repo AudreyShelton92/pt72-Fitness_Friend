035a274bb50fc1373f5b92d75f861d0a937a141d
AudreyShelton92 <83794598+AudreyShelton92@users.noreply.github.com>
2024-11-19 02:05:51 +0000

    navbar/bootstrap needs to be edited

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,38 +3,38 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Recipes</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Recipes</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Workouts</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Workouts</a>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Login/Sign Up
               </a>
-              <ul class="dropdown-menu">
-                <div class="mb-3 row">
-                  <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
-                  <div class="col-sm-10">
-                    <input type="text" class="form-control" id="inputEmail" value="">
+              <ul className="dropdown-menu">
+                <div className="mb-3 row">
+                  <label for="inputEmail" className="col-sm-2 col-form-label">Email</label>
+                  <div className="col-sm-10">
+                    <input type="text" className="form-control" id="inputEmail" value="">
                   </div>
                 </div>
-                <div class="mb-3 row">
-                  <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-                  <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword" value="">
+                <div className="mb-3 row">
+                  <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
+                  <div className="col-sm-10">
+                    <input type="password" className="form-control" id="inputPassword" value="">
                   </div>
                 </div>
               </ul>
@@ -45,3 +45,4 @@ export const Navbar = () => {
     </nav>
   );
 };
+//
